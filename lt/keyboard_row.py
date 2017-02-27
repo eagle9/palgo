992e58ed522285550c0b9385fcfee652332a2798
Shaun <voyieger@gmail.com>
2017-02-27 11:35:34 -0800

    modified:   lt/keyboard_row.py

--- lt/keyboard_row.py
+++ lt/keyboard_row.py
@@ -15,6 +15,7 @@ class Solution(object):
         """
         :type words: List[str]
         :rtype: List[str]
+	my own initial try, only beat .7%
         """
         res = []
         row_list = [set('qwertyyuiop'),set('asdfghjkl'),set('zxcvbnm')]
@@ -23,3 +24,18 @@ class Solution(object):
                 if self.checkHelper(word.lower(),row): 
                     res.append(word)
         return res
+
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        rs = map(set, ['qwertyuiop','asdfghjkl','zxcvbnm'])
+        ans = []
+        for word in words:
+            wset = set(word.lower())
+	    #check subset, nice use of python built in functions 
+            if any(wset <= rset for rset in rs):
+                ans.append(word)
+        return ans
+
