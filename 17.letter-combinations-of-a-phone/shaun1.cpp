39874bbac5cb818985381d3b5d82f3a00c4ad393
shaun <shaun@voyiger>
2019-06-10 13:07:43 -0700

    9

--- 17.letter-combinations-of-a-phone/shaun1.cpp
+++ 17.letter-combinations-of-a-phone/shaun1.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        
+        /*
         m['2'] = {'a','b','c'};
         m['3'] = {'d','e','f'};
         m['4'] = {'g','h','i'};
@@ -11,7 +11,15 @@ public:
         m['6'] = {'m','n','o'};
         m['7'] = {'p','q','r','s'};
         m['8'] = {'t','u','v'};
-        m['9'] = {'w','x','y','z'};
+        m['9'] = {'w','x','y','z'}; */
+        m = { {'2',{'a','b','c'}},
+             {'3',{'d','e','f'}},
+            {'4',{'g','h','i'}},
+             {'5',{'j','k','l'}},
+             {'6',{'m','n','o'}},
+             {'7',{'p','q','r','s'}},
+             {'8',{'t','u','v'}},
+             {'9',{'w','x','y','z'}}};
         int n = digits.size();
         
         vector<string> res;
